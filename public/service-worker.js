dde931ba9965d295499205752ba947314aa8dae7
Yoonseop Kang <e0engoon@gmail.com>
2020-03-14 07:16:24 +0000

    Update SW version

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,7 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v2'
+const PRECACHE = 'precache-v3'
 const RUNTIME = 'runtime'
 
 const PRECACHE_URLS = [
